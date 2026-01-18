35446291e04be63b22a7bc88c09ccd0cac2806ec
cheeksai <jacksonrmeans@gmail.com>
2026-01-18 15:00:12 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,50 +3,59 @@ import random
 import streamlit as st
 
 path = "icons"
-sounds1 = os.listdir(path)
-sounds = [f"{path}/{i}" for i in sounds1]
+files = os.listdir(path)
+sounds = [os.path.join(path, f) for f in files if f.lower().endswith(".mp3")]
+names = [
+    os.path.splitext(os.path.basename(p))[0].replace("-", " ").replace("_", " ")
+    for p in sounds
+]
 
-if "sound" not in st.session_state:
-    num = random.randint(0, len(sounds) - 1)
-    st.session_state.sound = sounds[num]
-    st.session_state.sound_answer = st.session_state.sound.replace("-", " ").replace("_", " ")[32:-4]
+if "index" not in st.session_state:
+    idx = random.randint(0, len(sounds) - 1)
+    st.session_state.index = idx
+    st.session_state.answer_name = names[idx]
     st.session_state.search = ""
     st.session_state.gave_up = False
-    st.session_state.played = False
 
-sound = st.session_state.sound
-sound_answer = st.session_state.sound_answer
-
-if st.session_state.played is False:
-    st.audio(sound)
-    st.session_state.played = True
+idx = st.session_state.index
+answer_name = st.session_state.answer_name
+answer_path = sounds[idx]
 
 st.title("Surviv.io Sound Effect")
+
+if st.button("Play sound"):
+    st.audio(answer_path)
 
 if st.button("Give Up"):
     st.session_state.gave_up = True
 
 if st.session_state.gave_up:
-    st.info(sound_answer)
+    st.info(answer_name)
 
-sounds2 = [i[32:-4].replace("-", " ").replace("_", " ") for i in sounds]
-
-search = st.text_input("Search & Choose:", st.session_state.search)
+search = st.text_input("Search & choose:", st.session_state.search)
 st.session_state.search = search
 
 if search == "":
-    filtered = sounds2
+    filtered_indices = list(range(len(names)))
 else:
-    filtered = [s for s in sounds2 if search.lower() in s.lower()]
+    filtered_indices = [
+        i for i, n in enumerate(names) if search.lower() in n.lower()
+    ]
 
-guess = st.selectbox("", filtered)
+if filtered_indices:
+    filtered_names = [names[i] for i in filtered_indices]
+    guess = st.selectbox("", filtered_names)
+else:
+    filtered_names = []
+    guess = None
+    st.write("No options to select")
 
 if st.button("Submit Guess"):
-    if guess == sound_answer:
+    if guess is not None and guess == answer_name:
         st.success("Correct!")
         st.balloons()
         if st.button("Play Again"):
             st.session_state.clear()
             st.rerun()
     else:
-        st.error(f"{guess} is incorrect")
+        st.error("Incorrect")
